65e0edd10ad167e23c1cc4e9d98378fc9f08e6f5
Josue Noe Soto Millán <zte-2000@hotmail.com>
2022-04-18 17:29:11 -0500

    Con imagen

--- lib/logIn.dart
+++ lib/logIn.dart
@@ -7,7 +7,7 @@ class logIn extends StatefulWidget {
 }
 
 class _logInState extends State<logIn> {
-  //variable declaration
+  //declaraciòn de variables
   String fraseIniciaSesion = "Inicia sesiòn";
 
   String fraseBienvenido = "¡Bienvenidos de nuevo!";
@@ -24,24 +24,20 @@ class _logInState extends State<logIn> {
     //Widgets
 
     //Widget para la imagen
-    /*final llamadaLogo = Container(
-      margin: EdgeInsets.only(top: 20),
-      alignment: Alignment.center,
-    );*/
-
     final llamadaLogo = Row(
-      children: [
+      children: <Widget>[
         Expanded(
           child: Image(
-              image:
-                  'https://avatars.githubusercontent.com/u/109951?s=400&v=4'),
+            image: AssetImage("assets/img/signos1.png"),
+            height: 100,
+          ),
         ),
       ],
     );
 
-    //Widget para mostrar un mensaje
+    //Widget para mostrar una frase
     final fraseLogIn = Container(
-      margin: EdgeInsets.only(top: 125, bottom: 40),
+      margin: EdgeInsets.only(top: 20, bottom: 40),
       alignment: Alignment.center,
       child: Column(
         children: <Widget>[
@@ -71,7 +67,6 @@ class _logInState extends State<logIn> {
               fontSize: 20.0,
               fontWeight: FontWeight.bold,
             ),
-            textAlign: TextAlign.left,
           ),
         ],
       ),
@@ -87,124 +82,150 @@ class _logInState extends State<logIn> {
             "\n¡Bienvenidos de nuevo!\n",
             style:
                 TextStyle(color: Color.fromRGBO(33, 47, 61, 1), fontSize: 14.0),
-            textAlign: TextAlign.left,
           ),
         ],
       ),
     );
 
-    //Widget para usuario y contraseña josjos
+    //Widget con formulario para ingresar
     final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-    final formLogIn = Column(children: <Widget>[
-      Text(
-        "Usuario",
-        style: TextStyle(
-            color: Color.fromRGBO(254, 102, 37, 1),
-            fontSize: 12.0,
-            fontWeight: FontWeight.bold),
-      ),
-      TextFormField(
-        decoration: const InputDecoration(
-            hintText: "mm.ximena367", hintStyle: TextStyle(fontSize: 12.0)),
-        validator: (String? value) {
-          if (value == null || value.isEmpty) {
-            return 'Ingresa tu usuario';
-          }
-          return null;
-        },
-      ),
-      //https://api.flutter.dev/flutter/widgets/Form-class.html
-
-      Text(
-        "\nPassword",
-        textAlign: TextAlign.left,
-        style: TextStyle(
-            color: Color.fromRGBO(254, 102, 37, 1),
-            fontSize: 12.0,
-            fontWeight: FontWeight.bold),
-      ),
-
-      TextFormField(
-        obscureText: true,
-        decoration: const InputDecoration(
-            hintText: "• • • • • • • • • • ",
-            hintStyle: TextStyle(fontSize: 12.0)),
-        validator: (String? value) {
-          if (value == null || value.isEmpty) {
-            return 'Ingresa ta contraseña';
-          }
-          return null;
-        },
-      ),
-      /*Padding(
-          padding: const EdgeInsets.symmetric(vertical: 25.0),
-          child: ElevatedButton(
-            onPressed: () {
-              // Validate will return true if the form is valid, or false if
-              // the form is invalid.
-              if (_formKey.currentState!.validate()) {
-                // Process data.
-              }
-            },
-            child: const Text("Ingresar"),
+    final formLogIn = Container(
+      margin: EdgeInsets.only(left: 35, right: 35),
+      child: Column(
+        children: <Widget>[
+          Text(
+            "Usuario",
+            style: TextStyle(
+                color: Color.fromRGBO(254, 102, 37, 1),
+                fontSize: 12.0,
+                fontWeight: FontWeight.bold),
           ),
-        ),*/
-      const SizedBox(height: 25),
-      ClipRRect(
-          borderRadius: BorderRadius.circular(10),
-          child: Stack(
-            children: <Widget>[
-              Positioned.fill(
-                child: Container(
-                  decoration: const BoxDecoration(
-                    color: Color.fromRGBO(254, 102, 37, 1),
+          TextFormField(
+            decoration: const InputDecoration(
+                hintText: "mm.ximena367", hintStyle: TextStyle(fontSize: 12.0)),
+            validator: (String? value) {
+              if (value == null || value.isEmpty) {
+                return 'Ingresa tu usuario';
+              }
+              return null;
+            },
+          ),
+          Text(
+            "\nPassword",
+            style: TextStyle(
+                color: Color.fromRGBO(254, 102, 37, 1),
+                fontSize: 12.0,
+                fontWeight: FontWeight.bold),
+          ),
+          TextFormField(
+            obscureText: true,
+            decoration: const InputDecoration(
+                hintText: "• • • • • • • • • • ",
+                hintStyle: TextStyle(fontSize: 12.0)),
+            validator: (String? value) {
+              if (value == null || value.isEmpty) {
+                return 'Ingresa ta contraseña';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: 25),
+          ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Stack(
+                children: <Widget>[
+                  Positioned.fill(
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        color: Color.fromRGBO(254, 102, 37, 1),
+                      ),
+                    ),
                   ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      padding: const EdgeInsets.only(
+                        left: 80,
+                        right: 80,
+                        top: 20,
+                        bottom: 20,
+                      ),
+                      primary: Colors.white,
+                      textStyle: const TextStyle(fontSize: 20),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      'Ingresar',
+                      style: TextStyle(fontSize: 14.0),
+                    ),
+                  )
+                ],
+              ))
+        ],
+      ),
+    );
+
+    //Widget de ayuda al usuario
+    final textAyuda2 = Column(
+      children: <Widget>[
+        SizedBox(height: 75),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "\n¿No tienes una cuenta?",
+              style: TextStyle(
+                fontSize: 12,
+              ),
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: const TextStyle(fontSize: 12),
+              ),
+              onPressed: () {},
+              child: const Text(
+                "\nRegistrate",
+                style: TextStyle(
+                  color: Color.fromRGBO(151, 151, 151, 1),
+                  decoration: TextDecoration.underline,
                 ),
               ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  padding: const EdgeInsets.only(
-                    left: 80,
-                    right: 80,
-                    top: 20,
-                    bottom: 20,
-                  ),
-                  primary: Colors.white,
-                  textStyle: const TextStyle(fontSize: 20),
-                ),
-                onPressed: () {},
-                child: const Text(
-                  'Ingresar',
-                  style: TextStyle(fontSize: 14.0),
-                ),
-              )
-            ],
-          ))
-    ]);
-
-    final textAyuda = Column(
-      children: <Widget>[
-        Row(
-          children: [
-            Text("¿No tienes una cuenta?"),
-            Text(" Registrate"),
+            ),
           ],
         ),
         Row(
-          children: [
-            Text("¿Olvidaste tu contraseña?"),
-            Text(" Contáctanos"),
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "\n¿Olvidaste tu contraseña?",
+              style: TextStyle(
+                fontSize: 12,
+              ),
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: const TextStyle(fontSize: 12),
+              ),
+              onPressed: () {},
+              child: const Text(
+                "\nContáctanos",
+                style: TextStyle(
+                  color: Color.fromRGBO(151, 151, 151, 1),
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
           ],
         )
       ],
     );
 
     final contenidoLogIn = Column(children: <Widget>[
+      llamadaLogo,
       fraseLogIn,
       fraseWelcome,
       fraseBienvenidos,
       formLogIn,
-      textAyuda,
+      textAyuda2,
     ]);
 
     return contenidoLogIn;
--- lib/logo.dart
+++ lib/logo.dart
@@ -1,4 +1,5 @@
-/*import 'package:flutter/material.dart';
+/*import 'package:elgood_de_mate/logIn.dart';
+import 'package:flutter/material.dart';
 
 class logoApp extends StatefulWidget {
   @override
@@ -6,6 +7,7 @@ class logoApp extends StatefulWidget {
 }
 
 class _logoAppState extends State<logoApp> {
+  //widget para mandar obtener y guardar la imagen
   final String imageLogo = "a";
   @override
   Widget build(BuildContext context) {
@@ -15,4 +17,9 @@ class _logoAppState extends State<logoApp> {
             image:
                 DecorationImage(image: AssetImage("assets/img/signos.png"))));
   }
-}*/
+}
+//Posible soluciòn, la direcciòn de la imagen se le asigna a una variable,
+//esa variable pasa a donde tenga que ir como un argumento y allà a la hora de
+//colocar AssetImage, colocamos la variable, para asì colocar la direcciòn en
+//dicha variable
+*/
