666973c71f51f089ea862ffe84c30bb1d3f81cfc
Josue Noe Soto Millán <zte-2000@hotmail.com>
2022-06-13 16:59:45 -0500

    Main Menu finalizado y cambio de nombres en ejes

--- lib/mainMenu.dart
+++ lib/mainMenu.dart
@@ -1,9 +1,8 @@
-import 'dart:html';
-
 import 'package:elgood_de_mate/helpers/margenes.dart';
 import 'newLogIn.dart';
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/percent_indicator.dart';
+import 'package:flutter/scheduler.dart' show timeDilation;
 
 class mainMenu extends StatefulWidget {
   @override
@@ -12,12 +11,9 @@ class mainMenu extends StatefulWidget {
 
 class _mainMenuState extends State<mainMenu> {
   @override
-  var ListItem = '';
-  //Definicion de la lista
-var x = 0;
-List<int> xList = [
-  xList(header: 'Contenido 1', body:"Contenido del bloque 1"),
-];
+  bool value = false;
+  double porcentaje = 0.19;
+  int unidadesCompletadas = 2;
   Widget build(BuildContext context) {
     // TODO: implement build
     var aMainMenu;
@@ -41,15 +37,15 @@ List<int> xList = [
                   ),
                   SizedBox(height: 10),
                   CircularPercentIndicator(
-                    radius: 60,
+                    radius: 55,
                     lineWidth: 8,
                     backgroundColor: Color.fromRGBO(54, 93, 137, 0.2),
                     progressColor: Color.fromRGBO(54, 93, 137, 1),
                     animation: true,
-                    percent: 0.19,
+                    percent: porcentaje,
                     circularStrokeCap: CircularStrokeCap.round,
                     center: Text(
-                      "2 de 7",
+                      "$unidadesCompletadas de 7",
                       style: TextStyle(
                         color: Color.fromRGBO(54, 93, 137, 1),
                         fontWeight: FontWeight.bold,
@@ -59,13 +55,276 @@ List<int> xList = [
                 ],
               ),
               SizedBox(height: 20),
-              Column(
-                children: [
-                  Container(
-                    child: 
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Color.fromRGBO(0, 0, 0, 0.5),
                   ),
-                ],
+                ),
+                child: ExpansionTile(
+                  backgroundColor: Color.fromRGBO(254, 102, 37, 0.5),
+                  collapsedBackgroundColor: Color.fromRGBO(254, 102, 37, 1),
+                  iconColor: Colors.black,
+                  collapsedIconColor: Colors.white,
+                  title: Container(
+                    padding: EdgeInsets.only(left: 20.0),
+                    child: Text(
+                      "1. Número, álgebra y variación",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  children: [
+                    Container(
+                      padding: EdgeInsets.only(bottom: 3.0),
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          textStyle: const TextStyle(fontSize: 12),
+                        ),
+                        onPressed: () {},
+                        child: Row(
+                          children: <Widget>[
+                            Checkbox(
+                                value: value,
+                                onChanged: (bool? value) {
+                                  setState(() {
+                                    value = value;
+                                  });
+                                }),
+                            Text(
+                              "Número",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 13,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(bottom: 3.0),
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          textStyle: const TextStyle(fontSize: 12),
+                        ),
+                        onPressed: () {},
+                        child: Row(
+                          children: <Widget>[
+                            Checkbox(
+                                value: value,
+                                onChanged: (bool? value) {
+                                  setState(() {
+                                    value = value;
+                                  });
+                                }),
+                            Text(
+                              "Adición y sustracción",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 13,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(bottom: 3.0),
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          textStyle: const TextStyle(fontSize: 12),
+                        ),
+                        onPressed: () {},
+                        child: Row(
+                          children: <Widget>[
+                            Checkbox(
+                                value: value,
+                                onChanged: (bool? value) {
+                                  setState(() {
+                                    value = value;
+                                  });
+                                }),
+                            Text(
+                              "Multiplicación y división",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 13,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
+              SizedBox(height: 12),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Color.fromRGBO(0, 0, 0, 0.5),
+                  ),
+                ),
+                child: ExpansionTile(
+                  backgroundColor: Color.fromRGBO(254, 102, 37, 0.5),
+                  collapsedBackgroundColor: Color.fromRGBO(254, 102, 37, 1),
+                  iconColor: Colors.black,
+                  collapsedIconColor: Colors.white,
+                  title: Container(
+                    padding: EdgeInsets.only(left: 20.0),
+                    child: Text(
+                      "2. Forma, espacio y división",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  children: [
+                    Container(
+                      padding: EdgeInsets.only(bottom: 3.0),
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          textStyle: const TextStyle(fontSize: 12),
+                        ),
+                        onPressed: () {},
+                        child: Row(
+                          children: <Widget>[
+                            Checkbox(
+                                value: value,
+                                onChanged: (bool? value) {
+                                  setState(() {
+                                    value = value;
+                                  });
+                                }),
+                            Text(
+                              "Ubicación espacial",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 13,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(bottom: 3.0),
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          textStyle: const TextStyle(fontSize: 12),
+                        ),
+                        onPressed: () {},
+                        child: Row(
+                          children: <Widget>[
+                            Checkbox(
+                                value: value,
+                                onChanged: (bool? value) {
+                                  setState(() {
+                                    value = value;
+                                  });
+                                }),
+                            Text(
+                              "Figuras",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 13,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(bottom: 3.0),
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          textStyle: const TextStyle(fontSize: 12),
+                        ),
+                        onPressed: () {},
+                        child: Row(
+                          children: <Widget>[
+                            Checkbox(
+                                value: value,
+                                onChanged: (bool? value) {
+                                  setState(() {
+                                    value = value;
+                                  });
+                                }),
+                            Text(
+                              "Cuerpos geométricos",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 13,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 12),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Color.fromRGBO(0, 0, 0, 0.5),
+                  ),
+                ),
+                child: ExpansionTile(
+                  backgroundColor: Color.fromRGBO(254, 102, 37, 0.5),
+                  collapsedBackgroundColor: Color.fromRGBO(254, 102, 37, 1),
+                  iconColor: Colors.black,
+                  collapsedIconColor: Colors.white,
+                  title: Container(
+                    padding: EdgeInsets.only(left: 20.0),
+                    child: Text(
+                      "3. Análisis de datos",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  children: [
+                    Container(
+                      padding: EdgeInsets.only(bottom: 3.0),
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          textStyle: const TextStyle(fontSize: 12),
+                        ),
+                        onPressed: () {},
+                        child: Row(
+                          children: <Widget>[
+                            Checkbox(
+                                value: value,
+                                onChanged: (bool? value) {
+                                  setState(() {
+                                    value = value;
+                                  });
+                                }),
+                            Text(
+                              "Estadística",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 13,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 12),
             ],
           ),
         ),
