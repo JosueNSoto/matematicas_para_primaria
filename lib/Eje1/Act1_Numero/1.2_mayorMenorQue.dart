b57000ceaa12a53ab1633efe2371b06b0b35d9ba
Josue Noe Soto Millán <zte-2000@hotmail.com>
2022-07-07 14:16:39 -0500

    actividad 2 finalizada

--- lib/Eje1/Act1_Numero/1.2_mayorMenorQue.dart
+++ lib/Eje1/Act1_Numero/1.2_mayorMenorQue.dart
@@ -1,6 +1,7 @@
 import 'package:elgood_de_mate/Eje1/Act1_Numero/1.3_igual.dart';
 import 'package:flutter/material.dart';
 import 'package:elgood_de_mate/helpers/margenes.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 
 class mayorMenorQue extends StatefulWidget {
   @override
@@ -71,7 +72,7 @@ class _mayorMenorQueState extends State<mayorMenorQue> {
                           RichText(
                             text: TextSpan(
                                 style: TextStyle(
-                                  color: Color.fromRGBO(254, 102, 37, 1),
+                                  color: Color.fromARGB(255, 0, 0, 0),
                                 ),
                                 children: [
                                   TextSpan(text: 'Identificar los signos "'),
@@ -80,6 +81,7 @@ class _mayorMenorQueState extends State<mayorMenorQue> {
                                     style: TextStyle(
                                       fontWeight: FontWeight.bold,
                                       fontSize: 15,
+                                      color: Color.fromRGBO(254, 102, 37, 1),
                                     ),
                                   ),
                                   TextSpan(text: '" y "'),
@@ -88,35 +90,44 @@ class _mayorMenorQueState extends State<mayorMenorQue> {
                                     style: TextStyle(
                                       fontWeight: FontWeight.bold,
                                       fontSize: 15,
+                                      color: Color.fromRGBO(254, 102, 37, 1),
                                     ),
                                   ),
                                   TextSpan(text: '"; que numeros es '),
                                   TextSpan(
                                     text: 'mayor que',
-                                    style:
-                                        TextStyle(fontWeight: FontWeight.bold),
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Color.fromRGBO(254, 102, 37, 1),
+                                    ),
                                   ),
                                   TextSpan(text: ' y '),
                                   TextSpan(
                                     text: 'menor que:',
-                                    style:
-                                        TextStyle(fontWeight: FontWeight.bold),
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Color.fromRGBO(254, 102, 37, 1),
+                                    ),
                                   ),
                                   TextSpan(
                                       text:
                                           '\n     •   El simbolo ">" significa '),
                                   TextSpan(
                                     text: 'mayor que',
-                                    style:
-                                        TextStyle(fontWeight: FontWeight.bold),
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Color.fromRGBO(254, 102, 37, 1),
+                                    ),
                                   ),
                                   TextSpan(
                                       text:
                                           '\n     •   El simbolo "<" significa '),
                                   TextSpan(
                                     text: 'menor que',
-                                    style:
-                                        TextStyle(fontWeight: FontWeight.bold),
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Color.fromRGBO(254, 102, 37, 1),
+                                    ),
                                   ),
                                 ]),
                           ),
@@ -138,23 +149,28 @@ class _mayorMenorQueState extends State<mayorMenorQue> {
                           Container(
                             child: Column(
                               children: [
-                                Image(
-                                  image:
-                                      AssetImage("assets/img/SolMayorQue.png"),
-                                  height: 150,
+                                Container(
+                                  child: Image(
+                                    image: AssetImage(
+                                        "assets/img/SolMayorQue.png"),
+                                    height: 150,
+                                  ),
                                 ),
                                 SizedBox(height: 5),
                                 RichText(
                                   text: TextSpan(
                                       style: TextStyle(
-                                        color: Color.fromRGBO(254, 102, 37, 1),
+                                        color: Color.fromARGB(255, 0, 0, 0),
                                       ),
                                       children: [
                                         TextSpan(text: "El sol es "),
                                         TextSpan(
                                           text: 'mayor que',
                                           style: TextStyle(
-                                              fontWeight: FontWeight.bold),
+                                            fontWeight: FontWeight.bold,
+                                            color:
+                                                Color.fromRGBO(254, 102, 37, 1),
+                                          ),
                                         ),
                                         TextSpan(text: " la luna"),
                                       ]),
@@ -167,33 +183,184 @@ class _mayorMenorQueState extends State<mayorMenorQue> {
                             child: Column(
                               children: [
                                 Center(
-                                  child: Image(
-                                    image: AssetImage(
-                                        "assets/img/LunaMenorQue.png"),
-                                    height: 150,
+                                  child: Container(
+                                    child: Image(
+                                      image: AssetImage(
+                                          "assets/img/LunaMenorQue.png"),
+                                      height: 150,
+                                    ),
                                   ),
                                 ),
                                 SizedBox(height: 5),
                                 RichText(
                                   text: TextSpan(
                                       style: TextStyle(
-                                        color: Color.fromRGBO(254, 102, 37, 1),
+                                        color: Color.fromARGB(255, 0, 0, 0),
                                       ),
                                       children: [
                                         TextSpan(text: "La luna es "),
                                         TextSpan(
                                           text: 'menor que',
                                           style: TextStyle(
-                                              fontWeight: FontWeight.bold),
+                                            fontWeight: FontWeight.bold,
+                                            color:
+                                                Color.fromRGBO(254, 102, 37, 1),
+                                          ),
                                         ),
                                         TextSpan(text: " el sol"),
                                       ]),
                                 ),
+                                SizedBox(height: 10),
                               ],
                             ),
                           ),
                         ],
                       ),
+                    ),
+                    Text('Pasa lo mismo con los números:'),
+                    SizedBox(height: 15),
+                    Column(
+                      children: [
+                        RichText(
+                          text: TextSpan(
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 0, 0, 0),
+                            ),
+                            children: [
+                              TextSpan(text: '    1.   20'),
+                              TextSpan(
+                                text: ' > ',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15,
+                                  color: Color.fromRGBO(254, 102, 37, 1),
+                                ),
+                              ),
+                              TextSpan(text: '15'),
+                              TextSpan(text: '\n    El número 20 '),
+                              TextSpan(
+                                text: 'es mayor que ',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15,
+                                  color: Color.fromRGBO(254, 102, 37, 1),
+                                ),
+                              ),
+                              TextSpan(text: '15\n'),
+                              TextSpan(text: '    2.   100'),
+                              TextSpan(
+                                text: ' > ',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15,
+                                  color: Color.fromRGBO(254, 102, 37, 1),
+                                ),
+                              ),
+                              TextSpan(text: '10'),
+                              TextSpan(text: '\n    El número 100 '),
+                              TextSpan(
+                                text: 'es mayor que ',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15,
+                                  color: Color.fromRGBO(254, 102, 37, 1),
+                                ),
+                              ),
+                              TextSpan(text: '10\n'),
+                              TextSpan(text: '    3.   5,000'),
+                              TextSpan(
+                                text: ' > ',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15,
+                                  color: Color.fromRGBO(254, 102, 37, 1),
+                                ),
+                              ),
+                              TextSpan(text: '2,500'),
+                              TextSpan(text: '\n    El número 5,000 '),
+                              TextSpan(
+                                text: 'es mayor que ',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15,
+                                  color: Color.fromRGBO(254, 102, 37, 1),
+                                ),
+                              ),
+                              TextSpan(text: '2,500'),
+                            ],
+                          ),
+                        ),
+                        SizedBox(height: 15),
+                        RichText(
+                          text: TextSpan(
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 0, 0, 0),
+                            ),
+                            children: [
+                              TextSpan(text: '     1.   5'),
+                              TextSpan(
+                                text: ' < ',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15,
+                                  color: Color.fromRGBO(254, 102, 37, 1),
+                                ),
+                              ),
+                              TextSpan(text: '10'),
+                              TextSpan(text: '\n     El número 5 '),
+                              TextSpan(
+                                text: 'es menor que ',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15,
+                                  color: Color.fromRGBO(254, 102, 37, 1),
+                                ),
+                              ),
+                              TextSpan(text: '10\n'),
+                              TextSpan(text: '     2.   999'),
+                              TextSpan(
+                                text: ' < ',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15,
+                                  color: Color.fromRGBO(254, 102, 37, 1),
+                                ),
+                              ),
+                              TextSpan(text: '1,000'),
+                              TextSpan(text: '\n     El número 999 '),
+                              TextSpan(
+                                text: 'es menor que ',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15,
+                                  color: Color.fromRGBO(254, 102, 37, 1),
+                                ),
+                              ),
+                              TextSpan(text: '1,000\n'),
+                              TextSpan(text: '     3.   9,000'),
+                              TextSpan(
+                                text: ' < ',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15,
+                                  color: Color.fromRGBO(254, 102, 37, 1),
+                                ),
+                              ),
+                              TextSpan(text: '10,000'),
+                              TextSpan(text: '\n     El número 9,000 '),
+                              TextSpan(
+                                text: 'es menor que ',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15,
+                                  color: Color.fromRGBO(254, 102, 37, 1),
+                                ),
+                              ),
+                              TextSpan(text: '10,000'),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
                     SizedBox(height: 10),
                     Text(
